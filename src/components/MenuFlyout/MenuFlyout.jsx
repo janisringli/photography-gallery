a59fd78e1b11d6ceb8f0f237e3ae782d7d49c2d7
Janis Ringli <janis.ringli@gmail.com>
2023-05-13 12:29:14 +0200

    add sidebar to flyout

--- src/components/MenuFlyout/MenuFlyout.jsx
+++ src/components/MenuFlyout/MenuFlyout.jsx
@@ -1,10 +1,21 @@
 import "./MenuFlyout.css"
+import Menu from "../../assets/svg/list.svg"
 
 function MenuFlyout(){
     return(
         <div className="menuFlyout-wrapper">
             <div className="menuFlyout-container">
-            <div className="menuFlyout-content"></div>
+            <div className="menuFlyout-content">
+                    <ul className="nav-content">
+                        <li className="nav-item">About</li>
+                        <li className="nav-item">Projects</li>
+                        <li className="nav-item">Photography</li>
+                    </ul>
+                    <div className="flyout-sidebar">
+                        <img className="flyout-menu-icon" src={Menu} alt="" />
+                        <div className="menu-close">Close</div>
+                    </div>
+            </div>
             <div className="menuFlyout-overlay-blur"></div>
             </div>
         </div>
