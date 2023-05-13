3c95f05be6c8e5106053714f78adeabc77b0217a
Janis Ringli <janis.ringli@gmail.com>
2023-05-13 11:53:17 +0200

    add basic layout for menuFlyout

--- src/Views/HomeView/HomeView.jsx
+++ src/Views/HomeView/HomeView.jsx
@@ -1,15 +1,19 @@
 import "./HomeView.css"
 import Header from "../../components/Header/Header.jsx"
 import janis from "../../assets/images/janis1.jpg"
+import MenuFlyout from "../../components/MenuFlyout/MenuFlyout"
 
 function HomeView(){
     return(
         <div className="homeView-container">
             <Header title="Home"></Header>
+            <MenuFlyout></MenuFlyout>
             <div className="homeView-content">
                 <div className="greetings-container">
-                <div className="greeting"><p className="image-overlap">Hello There,</p> I'm Janis Ring<span className="image-overlap">li</span>
-
+                <div className="greeting">
+                    <p className="image-overlap">Hello There,</p>
+                     I'm Janis Ring
+                     <span className="image-overlap">li</span>
                     <img class="intro-photo" src={janis} alt="" />
                     </div>
                 </div>
--- src/components/MenuFlyout/MenuFlyout.jsx
+++ src/components/MenuFlyout/MenuFlyout.jsx
@@ -2,7 +2,12 @@ import "./MenuFlyout.css"
 
 function MenuFlyout(){
     return(
-        <div className="menuFlyout-wrapper"></div>
+        <div className="menuFlyout-wrapper">
+            <div className="menuFlyout-container">
+            <div className="menuFlyout-content"></div>
+            <div className="menuFlyout-overlay-blur"></div>
+            </div>
+        </div>
     )
 }
 export default MenuFlyout
