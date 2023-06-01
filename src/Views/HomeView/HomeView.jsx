23fcbf274c602f05deee1be0f4883ad0daac0ba4
Janis Ringli <janis.ringli@gmail.com>
2023-06-01 10:30:31 +0200

    optimised images for web

--- src/Views/HomeView/HomeView.jsx
+++ src/Views/HomeView/HomeView.jsx
@@ -1,7 +1,6 @@
 import "./HomeView.css";
 import Header from "../../components/Header/Header.jsx";
 import janis from "../../assets/images/janis1.jpg";
-import MenuFlyout from "../../components/MenuFlyout/MenuFlyout";
 
 function HomeView() {
   return (
@@ -10,10 +9,9 @@ function HomeView() {
       <div className="homeView-content">
         <div className="greetings-container">
           <div className="greeting">
-            <p className="image-overlap">Hello there,</p>
-            I'm Janis Ring
+            <p className="image-overlap">Hello there,</p>I'm Janis Ring
             <span className="image-overlap">li</span>
-            <img class="intro-photo" src={janis} alt="" />
+            <img className="intro-photo" src={janis} alt="" />
           </div>
         </div>
       </div>
