b355ded12a44722f9fd1349930fdff09cc3baa8a
Janis Ringli <janis.ringli@gmail.com>
2023-06-03 23:52:08 +0200

    add new Component and update old structure

---
+++ src/components/SidebarTitle/SidebarTitle.jsx
@@ -0,0 +1,12 @@
+import "./SidebarTitle.css"
+function SidebarTitle(){
+    return (
+        <div className="title-section">
+          <div className="about-title">About</div>
+          <div className="footer">
+           
+          </div>
+        </div>
+    )
+}
+export default SidebarTitle
