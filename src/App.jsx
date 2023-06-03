61c4c715c6e7858da59ca1fede94896d24cbf1a5
Janis Ringli <janis.ringli@gmail.com>
2023-06-04 00:32:56 +0200

    add new View to routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomeView from "./Views/HomeView/HomeView.jsx";
 import AboutView from "./Views/AboutView/AboutView.jsx";
 import PhotoGalleryView from "./Views/PhotoGalleryView/PhotoGalleryView.jsx";
+import NatureView from "./Views/NatureView/NatureView.jsx";
+
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
           <Route path="/about" element={<AboutView />} />
           <Route path="/" element={<HomeView />} />
           <Route path='/photogallery' element={<PhotoGalleryView />} />
+          <Route path="/photo/nature/" element={<NatureView/>} />
         </Routes>
       </div>
     </Router>
