a96e378b460cd3adc20c566100efecd7a471c0d7
Janis Ringli <janis.ringli@gmail.com>
2023-06-07 18:54:20 +0200

    add impressum and datenschutz to router and flyout module

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import AboutView from "./Views/AboutView/AboutView.jsx";
 import PhotoGalleryView from "./Views/PhotoGalleryView/PhotoGalleryView.jsx";
 import NatureView from "./Views/NatureView/NatureView.jsx";
 import FlowersView from "./Views/FlowersView/FlowersView.jsx";
-
+import ImpressumView from "./Views/ImpressumView/ImpressumView.jsx";
+import DatenschutzView from "./Views/DatenschutzView/DatenschutzView.jsx";
 
 function App() {
   return (
@@ -13,10 +14,11 @@ function App() {
         <Routes>
           <Route path="/about" element={<AboutView />} />
           <Route path="/" element={<HomeView />} />
-          <Route path='/photogallery' element={<PhotoGalleryView />} />
-          <Route path="/photo/nature/" element={<NatureView/>} />
-          <Route path="/photo/flowers/" element={<FlowersView/>} />
-
+          <Route path="/photogallery" element={<PhotoGalleryView />} />
+          <Route path="/photo/nature/" element={<NatureView />} />
+          <Route path="/photo/flowers/" element={<FlowersView />} />
+          <Route path="/impressum" element={<ImpressumView />} />
+          <Route path="/datenschutz" element={<DatenschutzView />} />
         </Routes>
       </div>
     </Router>
