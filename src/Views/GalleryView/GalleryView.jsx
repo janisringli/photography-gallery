02f9442ba56be79f83b4786664fe14b1c4261ee1
Janis Ringli <janis.ringli@gmail.com>
2023-06-14 15:08:56 +0200

    add clean gallery with photos from unsplash

---
+++ src/Views/GalleryView/GalleryView.jsx
@@ -0,0 +1,98 @@
+import Header from "../../components/Header/Header";
+import SidebarTitle from "../../components/SideBarTitle/SideBarTitle";
+import "./GalleryView.css";
+
+function GalleryView() {
+  return (
+    <div className="gallery-flex">
+    
+<div className="gallery-container">
+<Header title="Gallery"></Header>
+<div className="gallery-content">
+    <div className="gallery-row">
+    <div className="gallery-item">
+      <img src="https://source.unsplash.com/random?people" alt="Image 3" />
+    </div>
+    <div className="gallery-item">
+      <img src="https://source.unsplash.com/random?landscape" alt="Image 3" />
+    </div>
+    <div className="gallery-item">
+      <img src="https://source.unsplash.com/random?face" alt="Image 3" />
+    </div>
+    <div className="gallery-item">
+      <img src="https://source.unsplash.com/random?skyline" alt="Image 3" />
+    </div>
+    <div className="gallery-item">
+      <img src="https://source.unsplash.com/random?sunset" alt="Image 3" />
+    </div>
+    <div className="gallery-item">
+      <img src="https://source.unsplash.com/random?calm" alt="Image 3" />
+    </div>
+    <div className="gallery-item">
+      <img src="https://source.unsplash.com/random?group" alt="Image 3" />
+    </div>
+    <div className="gallery-item">
+      <img src="https://source.unsplash.com/random?tech" alt="Image 3" />
+    </div>
+
+    </div>
+    <div className="gallery-row">
+    <div className="gallery-item">
+      <img src="https://source.unsplash.com/random?apple" alt="Image 3" />
+    </div>
+    <div className="gallery-item">
+      <img src="https://source.unsplash.com/random?presentation" alt="Image 3" />
+    </div>
+    <div className="gallery-item">
+      <img src="https://source.unsplash.com/random?bread" alt="Image 3" />
+    </div>
+    <div className="gallery-item">
+      <img src="https://source.unsplash.com/random?coffee" alt="Image 3" />
+    </div>
+    <div className="gallery-item">
+      <img src="https://source.unsplash.com/random?video" alt="Image 3" />
+    </div>
+    <div className="gallery-item">
+      <img src="https://source.unsplash.com/random?google" alt="Image 3" />
+    </div>
+    <div className="gallery-item">
+      <img src="https://source.unsplash.com/random?nasa" alt="Image 3" />
+    </div>
+    <div className="gallery-item">
+      <img src="https://source.unsplash.com/random?moon" alt="Image 3" />
+    </div>
+
+    </div>
+    <div className="gallery-row">
+    <div className="gallery-item">
+      <img src="https://source.unsplash.com/random?web" alt="Image 3" />
+    </div>
+    <div className="gallery-item">
+      <img src="https://source.unsplash.com/random?still" alt="Image 3" />
+    </div>
+    <div className="gallery-item">
+      <img src="https://source.unsplash.com/random?model" alt="Image 3" />
+    </div>
+    <div className="gallery-item">
+      <img src="https://source.unsplash.com/random?current" alt="Image 3" />
+    </div>
+    <div className="gallery-item">
+      <img src="https://source.unsplash.com/random?football" alt="Image 3" />
+    </div>
+    <div className="gallery-item">
+      <img src="https://source.unsplash.com/random?badminton" alt="Image 3" />
+    </div>
+    <div className="gallery-item">
+      <img src="https://source.unsplash.com/random?club" alt="Image 3" />
+    </div>
+    <div className="gallery-item">
+      <img src="https://source.unsplash.com/random?portrait" alt="Image 3" />
+    </div>
+
+    </div>
+</div>
+  </div>
+  </div>
+    );
+}
+export default GalleryView
