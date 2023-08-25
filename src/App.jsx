db726d59981f2c61ff2d9d32f224ce8dae0a1b88
Janis Ringli <janis.ringli@gmail.com>
2023-08-25 21:07:02 +0200

    add gallery to prod

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomeView from "./Views/HomeView/HomeView.jsx";
 import AboutView from "./Views/AboutView/AboutView.jsx";
 import PhotoGalleryView from "./Views/PhotoGalleryView/PhotoGalleryView.jsx";
-import NatureView from "./Views/NatureView/NatureView.jsx";
-import FlowersView from "./Views/FlowersView/FlowersView.jsx";
 import ImpressumView from "./Views/ImpressumView/ImpressumView.jsx";
 import DatenschutzView from "./Views/DatenschutzView/DatenschutzView.jsx";
 import ServiceView from "./Views/ServiceView/ServiceView.jsx";
@@ -19,8 +17,6 @@ function App() {
           <Route path="/about" element={<AboutView />} />
           <Route path="/" element={<HomeView />} />
           <Route path="/photogallery" element={<PhotoGalleryView />} />
-          <Route path="/photo/nature/" element={<NatureView />} />
-          <Route path="/photo/flowers/" element={<FlowersView />} />
           <Route path="/datenschutz" element={<ImpressumView />} />
           <Route path="/impressum" element={<DatenschutzView />} />
           <Route path="/service" element={<ServiceView />} />
